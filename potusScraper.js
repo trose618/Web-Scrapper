1ee1f6ae64d1e152f11046c15ccdedd4a404a462
trose618 <trose618@gmail.com>
2019-03-26 12:12:51 -0400

    prints list of links to each president's wiki page

--- potusScraper.js
+++ potusScraper.js
@@ -6,8 +6,15 @@ rp(url)
     .then(function (html) {
         //success!
         // console.log(html); // << prints raw html of site
-        console.log($('big > a', html).length);  // $ is jquery for document. This line is printing the length of the list of elements that are an 'a' tag nested in 'big' tags
-        console.log($('big > a', html)); //this line prints the list of each big tag with a nested a tag
+        // console.log($('big > a', html).length);  // $ is jquery for document. This line is printing the length of the list of elements that are an 'a' tag nested in 'big' tags
+        // console.log($('big > a', html)); //this line prints the list of each big tag with a nested a tag
+        const wikiUrls = []; // array for storing the urls to each president's wiki page
+
+        for (let i = 0; i < 45; i++) { // for loop going through length of array.
+            wikiUrls.push($('big > a', html)[i].attribs.href); // body accesses attribute list of current index element to get href link and push into wikiUrls array
+        }
+
+        console.log(wikiUrls);
     })
     .catch(function (err) {
         //handle error
